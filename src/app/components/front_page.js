0e4a60a27b95ac329b3a851cae6a2355653e746c
Richard <richard_stephens@hotmail.co.uk>
2023-08-22 15:21:24 +0100

    added additional fetch

--- src/app/components/front_page.js
+++ src/app/components/front_page.js
@@ -4,15 +4,17 @@ import UserPanel from "./userPanels";
 import Image from "next/image";
 import styles from "../page.module.css";
 import Graphs from "./graph";
+import MonthCalc from "./month_calcs";
 //Images
 import leaderboard from "./images/leaderboard.svg";
 import starConstellation from "./images/starConstellation.svg";
-
-import starburst from "./images/starburst.svg";
+import starburst_background from "./images/starburst_background.svg";
+import calendar from "./images/calendar.svg";
 
 export default function Front(props) {
   const { sortedValues, stockPrices } = props;
   const [submitted, setSubmitted] = useState(false);
+  const [month, setMonth] = useState(false);
 
   const handleSubmittedChange = (value) => {
     setSubmitted(value + 1);
@@ -21,48 +23,60 @@ export default function Front(props) {
     setSubmitted(false);
   };
 
-  return !submitted ? (
-    <div className={styles.outer_container}>
-      <div className={styles.container}>
-        <div className={styles.header_leaderboard}>
-          <Image
-            priority
-            src={leaderboard}
-            alt="Leaderboard header"
-            width={426}
-            height={84}
-          />
-        </div>
-        {/* <Image
-          className={styles.starburst}
-          fill
-          src={starburst}
-          alt="Starburst"
-        /> */}
-        <div className={styles.inner_container}>
-          <Image
-            className={styles.stars}
-            priority
-            src={starConstellation}
-            alt="Stars"
-          />
+  const handleMonthClick = () => {
+    setMonth(true);
+  };
 
-          <div className={styles.scorecard_container}>
-            <UserPanel
-              sortedValues={sortedValues}
-              stockPrices={stockPrices}
-              onSubmittedChange={handleSubmittedChange}
+  return !month ? (
+    !submitted ? (
+      <div className={styles.outer_container}>
+        <div className={styles.container}>
+          <div className={styles.header_leaderboard}>
+            <Image
+              priority
+              src={leaderboard}
+              alt="Leaderboard header"
+              width={426}
+              height={84}
             />
+            <div className={styles.calendar} onClick={handleMonthClick}>
+              <Image src={calendar} alt="Calendar gif" height={30} width={30} />
+            </div>
+          </div>
+          <div className={styles.inner_container}>
+            <Image
+              className={styles.stars}
+              priority
+              src={starConstellation}
+              alt="Stars"
+            />
+            <div className={styles.starburst_background}>
+              <Image
+                fill
+                priority
+                src={starburst_background}
+                alt="Background light rays"
+              />
+            </div>
+            <div className={styles.scorecard_container}>
+              <UserPanel
+                sortedValues={sortedValues}
+                stockPrices={stockPrices}
+                onSubmittedChange={handleSubmittedChange}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    ) : (
+      <Graphs
+        sortedValues={sortedValues}
+        stockPrices={stockPrices}
+        user={submitted}
+        onArrowClick={onArrowClickFunc}
+      />
+    )
   ) : (
-    <Graphs
-      sortedValues={sortedValues}
-      stockPrices={stockPrices}
-      user={submitted}
-      onArrowClick={onArrowClickFunc}
-    />
+    <MonthCalc />
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,3 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
-// import PlotlyComponent from "./components/plotly";
-// import GraphCard from "./components/scatter";
-// import Static from "./components/static_chart";
 import Calc from "./components/calculate";
 
 export default function Home() {
