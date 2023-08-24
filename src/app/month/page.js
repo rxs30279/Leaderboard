df1f7e430b9746d7ecc154fe8f60fcb1c05f6aa3
Richard <richard_stephens@hotmail.co.uk>
2023-08-24 13:18:08 +0100

    added API for loop to monthly

--- src/app/month/page.js
+++ src/app/month/page.js
@@ -132,24 +132,26 @@ export default async function MonthCalc() {
   );
 
   async function fetchData(shares) {
+    const apiKeys = process.env.API_KEYS.split(",");
     const url = `https://yfapi.net/v8/finance/spark?interval=1d&range=1mo&symbols=${shares}`;
-    const options = {
-      method: "GET",
-      cache: "no-store",
-      headers: {
-        "x-api-key": "sudEH6c1SN2LlLSAwYrS68wQ5p521GSi3BkmFaAO",
-      },
-    };
+    for (const apiKey of apiKeys) {
+      const options = {
+        method: "GET",
+        cache: "no-store",
+        headers: {
+          "x-api-key": apiKey,
+        },
+      };
 
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      return result;
-    } catch (error) {
-      console.error(error);
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        return result;
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
-
   async function callApi() {
     const companies = [
       "GFI%2CRR.L%2CKAPE.L%2CSCT.L%2CGAW.L%2CBYIT.L%2CCVSG.L%2CSGE.L%2COXB.L%2CSHOE.L%2CKMR.L%2CITV.L%2CDRX.L%2CCRW.L%2CCEY.L%2CPSN.L%2CSTAN.L%2CLLOY.L%2CSDP.L%2CHL.L",
